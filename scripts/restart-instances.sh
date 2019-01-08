a148b5e6597d9f6bce6355ddd4e9180921e4d7e8
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-01-08 15:13:59 +0100

    add restart-instances script

---
+++ scripts/restart-instances.sh
@@ -0,0 +1,8 @@
+./bin/instance1-anthropology restart
+./bin/instance1-cpb restart
+./bin/zeoserver-entomo restart
+./bin/instance1-geology restart
+./bin/instance1-invertebrates restart
+./bin/instance1-paleontology restart
+./bin/zeoserver-projects restart
+./bin/instance1-vertebrates restart

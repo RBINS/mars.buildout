ea71e0ab23ba1cb315079fbcda4f92ce95705a7d
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-03-04 10:28:30 +0100

    pep8 prod-to-dev

--- scripts/prod-to-dev.py
+++ scripts/prod-to-dev.py
@@ -2,11 +2,11 @@
 Script to be launched on development instance
 once Data.fs has been recovered from production
 """
-from plone import api
-from zope.component.hooks import setSite
-from transaction import commit
 import logging
-# from ecreall.helpers.upgrade.interfaces import IUpgradeTool
+
+from plone import api
+from transaction import commit
+from zope.component.hooks import setSite
 
 logger = logging.getLogger('prod-to-dev')
 site = app.Plone
@@ -19,17 +19,5 @@ with api.env.adopt_user(username="admin"):
         commit()
 
     site.portal_purgepolicy.maxNumberOfVersionsToKeep = 0
-    # delete all file and image objects
-    # tool = IUpgradeTool(site.portal_setup)
-    #
-    # def delete_object(obj, path):
-    #     api.content.delete(obj=obj, check_linkintegrity=False)
-    #
-    #
-    # tool.migrateContent(('Image', 'File'), delete_object,
-    #                     commit=True, nofail=False)
-    #
-    # tool.reindexContents(tool.portal.portal_types.keys(),
-    #                      commit=True, nofail=False)
 
 commit()

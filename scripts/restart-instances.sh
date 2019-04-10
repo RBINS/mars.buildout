2a2ba05752527e74fd7ad6d94cc7bf7871aa0382
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-04-10 14:33:16 +0200

    update restart-instances script

--- scripts/restart-instances.sh
+++ scripts/restart-instances.sh
@@ -1,1 +1,4 @@
-./bin/supervisorctl restart instance1 instance1-anthropology instance1-geology instance1-entomo instance1-invertebrates instance1-paleontology instance1-projects instance1-vertebrates instance1-cpb
+./bin/supervisorctl restart instance1 \
+    instance1-anthropology instance1-geology instance1-entomo instance1-invertebrates \
+    instance1-paleontology instance1-projects instance1-vertebrates instance1-cpb \
+    instance1-geology-biblio instance1-habitats

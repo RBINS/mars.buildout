9a32248909dea8342347c0ee3723b3fec4a80218
Gagaro <gagaro42@gmail.com>
2020-10-19 11:18:03 +0200

    fix #20780: Dédoubler le site ssh-geology en ssh-geology-archives

--- scripts/restart-instances.sh
+++ scripts/restart-instances.sh
@@ -1,4 +1,4 @@
 ./bin/supervisorctl restart instance1 \
-    instance1-anthropology instance1-geology instance1-entomo instance1-invertebrates \
+    instance1-anthropology instance1-geology instance1-geology-archives instance1-entomo instance1-invertebrates \
     instance1-paleontology instance1-projects instance1-vertebrates instance1-cpb \
     instance1-geology-biblio instance1-habitats instance1-digit instance1-ltp instance1-nagoya
